6692e9013225e4892037d37d436291855ad6bbd4
Anastasia <akryzhanivska2020@itcollege.lviv.ua>
2024-02-27 09:34:14 +0200

    static list

---
+++ src/app/modal-window/modal-window.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-window',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-window.component.html',
+  styleUrl: './modal-window.component.css'
+})
+export class ModalWindowComponent {
+
+}
---
+++ src/app/weather-display/weather-display.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-display',
+  standalone: true,
+  imports: [],
+  templateUrl: './weather-display.component.html',
+  styleUrl: './weather-display.component.css'
+})
+export class WeatherDisplayComponent {
+
+}
