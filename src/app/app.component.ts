a7d58ae4c725439eda4af376220e1dcbdb196e4d
Anastasia <akryzhanivska2020@itcollege.lviv.ua>
2024-02-29 20:48:29 +0200

    cleaning up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TripListComponent } from './trip-list/trip-list.component';
-import { TripDetailsComponent } from './trip-details/trip-details.component';
 import { ModalWindowComponent } from './modal-window/modal-window.component';
-import { WeatherDisplayComponent } from './weather-display/weather-display.component';
 import { HttpClientModule } from '@angular/common/http';
-import { EnvironmentInjector } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -20,8 +17,6 @@ import { FormsModule } from '@angular/forms';
     RouterOutlet,
     TripListComponent,
     ModalWindowComponent,
-    WeatherDisplayComponent,
-    TripDetailsComponent,
     HttpClientModule,
     FormsModule,
   ],
