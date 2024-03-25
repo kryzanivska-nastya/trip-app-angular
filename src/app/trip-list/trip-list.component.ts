4b1e3a02545a2f5b925f222a6a1b48affdd61427
Anastasia <155824290+kryzanivska-nastya@users.noreply.github.com>
2024-03-25 11:00:06 +0200

    upd

--- src/app/trip-list/trip-list.component.ts
+++ src/app/trip-list/trip-list.component.ts
@@ -65,15 +65,6 @@ export class TripListComponent {
     });
   }
 
-  selectTrip(trip: any, index: number) {
-    this.selectedTrip = trip;
-    this.tripSelected = true;
-    this.fetchForecast(trip.destination, trip.startDate, trip.endDate);
-    this.fetchTodayForecast(trip.destination);
-    this.calculateCountdown(trip.startDate);
-    this.lastSelectedIndex = index;
-  }
-
   fetchForecast(city: string, startDate: string, endDate: string) {
     const apiKey = '5JPNVGTXYQPBQ8YBGBNJJ2SYQ';
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/${startDate}/${endDate}?unitGroup=metric&include=days&key=${apiKey}&contentType=json`;
@@ -91,6 +82,15 @@ export class TripListComponent {
       .catch((error) => {
         console.error('Error fetching forecast:', error);
       });
+  }
+
+  selectTrip(trip: any, index: number) {
+    this.selectedTrip = trip;
+    this.tripSelected = true;
+    this.fetchForecast(trip.destination, trip.startDate, trip.endDate);
+    this.fetchTodayForecast(trip.destination);
+    this.calculateCountdown(trip.startDate);
+    this.lastSelectedIndex = index;
   }
 
   fetchTodayForecast(city: string) {
